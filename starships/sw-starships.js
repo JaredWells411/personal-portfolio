373c14e9460d4b96e56d151ca3382d1e19125bb4
Jared <jaredwells32@gmail.com>
2021-11-09 12:28:02 -0700

    October 28th Makeup

---
+++ starships/sw-starships.js
@@ -0,0 +1,29 @@
+import {starships} from "../data/starships.js"
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.displaySection')
+
+function populateView(starships) {
+    starships.forEach(starship => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        let listItem = document.createElement('li')
+        listItem.textContent = starship.name
+        anchorWrap.addEventListener('click', () => {
+            populateShipView(starship)
+        })
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+    })
+}
+
+populateView(starships)
+
+function populateShipView(shipData) {
+    console.log(`Blah Blah Blah ${shipData.name}`)
+    let shipImage = deocument.createElement('img')
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/5.jpg`
+    shipView.appendChild(shipImage)
+}
