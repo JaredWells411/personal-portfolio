48c439acf35e362fdccf64e5540f83736f0c3abf
Jared <jaredwells32@gmail.com>
2021-12-02 10:48:41 -0700

    Made Create Your Own Pokemon Framework

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,3 +1,4 @@
+import { removeChildren } from '../utility/index.js'
 async function getAPIData(url) {
     try {
         return fetch(url).then((data) => data.json())
@@ -6,19 +7,30 @@ async function getAPIData(url) {
     }
 }
 
-function loadPokemon(offset, limit) {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`).then((data) => {
-    console.log(data.results)
+function loadPokemon(offset = 0, limit = 25) {
+    removeChildren(pokeGrid)
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`).then(async (data) => {
     for (const pokemon of data.results) {
-        getAPIData(pokemon.url).then(pokeData => populatePokeCards(pokeData))
+        await getAPIData(pokemon.url).then((pokeData) => populatePokeCard(pokeData))
     }
 })}
 
 const pokeGrid = document.querySelector('.pokeGrid')
 const loadButton = document.querySelector('.loadPokemon')
-loadButton.addEventListener('click', () => loadPokemon())
+loadButton.addEventListener('click', () => loadPokemon(800, 50))
+const newButton = document.querySelector('.newPokemon')
+newButton.addEventListener('click', () => {
+    let pokeName = prompt('What is the name of your new Pokemon?')
+    let pokeHeight = prompt('What is the height of your Pokemon?')
+    let pokeWeight = prompt('What is the weight of your Pokemon?')
+    let pokeAbilities = prompt('What are your pokemon abilities? (use a coma seperated list)')
+    let newPokemon = new Pokemon (pokeName, pokeHeight, pokeWeight, pokeAbilities)
+    console.log(newPokemon)
+    populatePokeCard(newPokemon)
+    
+})
 
-function populatePokeCards(singlePokemon) {
+function populatePokeCard(singlePokemon) {
     const pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
     const pokeCard = document.createElement('div')
@@ -63,3 +75,14 @@ function populateCardBack(pokemon) {
     pokeBack.appendChild(abilityList)
     return pokeBack
 }
+
+class Pokemon {
+    constructor(name, height, weight, abilities) {
+        this.name = name,
+        this.height = height,
+        this.weight = weight,
+        this.abilities = abilities
+    }
+}
+
+let newPokemon = new Pokemon('Thoremon', 243, 3754)
