f315da5d185c59b9a76d3b7ce8fb571c510cf328
Jared <jaredwells32@gmail.com>
2021-12-13 17:54:53 -0700

    Added To Box

--- utility/index.js
+++ utility/index.js
@@ -13,7 +13,7 @@ export function removeChildren(container) {
     }
 }
 
-const urlArray = ['https://youtu.be/6Fdpuhib4Dc?t=33', 'https://youtu.be/U9t-slLl30E?t=14', 'https://youtu.be/dQw4w9WgXcQ', 'https://www.youtube.com/watch?v=O6SEXp3-lo8', 'https://www.youtube.com/watch?v=CPRTfs9PYHU', 'https://youtu.be/FKXharUfPVQ?t=2', 'https://media.makeameme.org/created/when-you-trick-5b0431.jpg', 'https://lh3.googleusercontent.com/proxy/ELaLCi97-Izz1PMofEAs2dIhpr5k_2sgvM1O8kq7ptiN54G30y04wGPJiIfcl5uWE4ujQdd6Hv_5yprpM1p0KJ01R_cMsbxjlAnpGzkZJKby5Dm7Ct4TAl4CPGb2-fkH1sPueh-obC23Y8i7HEuPqCfVblsm1PXR03A', 'https://i.annihil.us/u/prod/marvel/i/mg/f/80/5da4ca693ffa6/clean.jpg']
+const urlArray = ['https://youtu.be/6Fdpuhib4Dc?t=33', 'https://youtu.be/U9t-slLl30E?t=14', 'https://youtu.be/dQw4w9WgXcQ', 'https://www.youtube.com/watch?v=O6SEXp3-lo8', 'https://www.youtube.com/watch?v=CPRTfs9PYHU', 'https://youtu.be/FKXharUfPVQ?t=2', 'https://media.makeameme.org/created/when-you-trick-5b0431.jpg', 'https://i.annihil.us/u/prod/marvel/i/mg/f/80/5da4ca693ffa6/clean.jpg', 'https://youtu.be/o0u4M6vppCI?t=6']
 
 const memes = document.querySelector ('#memes')
 memes.addEventListener('click', () => {
